a48b8fd975b01c7875069bcbc2a2113eb1952de6
foxworld <foxworld@gmail.com>
2024-05-20 21:53:29 +0900

    safe args 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("androidx.navigation.safeargs")
 }
 
 android {
@@ -42,4 +43,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }
--- app/src/main/java/kr/co/ksnet/navigation_exam/MainFragment.java
+++ app/src/main/java/kr/co/ksnet/navigation_exam/MainFragment.java
@@ -69,10 +69,13 @@ public class MainFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+//        ActionMainFragmentToSecondFragment action = MainFragmentDirections.actionMainFragmentToSecondFragment();
+//        action.setVar("Hello!!!");
+
         view.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Navigation.findNavController(v).navigate(R.id.action_mainFragment_to_secondFragment);
+                Navigation.findNavController(v).navigate(action);
             }
         });
     }
